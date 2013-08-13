ae74d0acd92ad23dc60d69cdff546a0c31a9b40e
sweslo17 <sweslo17@gmail.com>
2013-08-13 22:07:40 +0800

    clean code

--- server/src/index.js
+++ server/src/index.js
@@ -1,12 +1,6 @@
 var server = require("./server");
 var router = require("./router");
 var watchdog_util = require("./watchdog-util");
-//var test2 = require("./test2"); 
-global.test = 5;
-//util.test();
-//util.print("aaa");
-//test2.test();
-//test2.print("bbb");
 var http = require("http");
 var data_file_root = "../data/";
 var HashMap = require('hashmap').HashMap;
@@ -24,31 +18,21 @@ function dateToYMD(date) {
 	return '' + y  + (m<=9 ? '0' + m : m) + (d <= 9 ? '0' + d : d);
 }
 function parseYMD(str) {
-	//console.log(str);
 	if(!/^(\d){8}$/.test(str)) return "invalid date";
 	var y = str.substr(0,4),
 		m = str.substr(4,2),
 		  d = str.substr(6,2);
-	//console.log(y+','+m+','+d)
 	return new Date(y,m-1,d);
 }
 var  Handle = {};
 Handle['/query'] = requestHandler.query;
 Handle['/verify'] = userUtils.verify;
 
-//Handle['/get_stat'] = requestHandler.get_stat;
 global.user_list = JSON.parse(fs.readFileSync(data_file_root+'user.json', 'utf8'));
 global.term_list = JSON.parse(fs.readFileSync(data_file_root+'term.json', 'utf8'));
 global.config = JSON.parse(fs.readFileSync(data_file_root+'conf.json', 'utf8'));
-//console.log(JSON.parse(fs.readFileSync(data_file_root+'pool.calander.json', 'utf8'))); 
 global.pool = {};
 global.pool.calander = JSON.parse(fs.readFileSync(data_file_root+'pool.calander.json', 'utf8'));
-//watchdog_util.add_term("sweslo17@gmail.com","abc");
-//watchdog_util.remove_term("sweslo17@gmail.com","洪仲丘");
-//console.log(JSON.stringify(global.user_list));
-//console.log(JSON.stringify(global.term_list));
-//console.log(JSON.stringify(watchdog_util.poll("email","calander"),undefined,4));
-//console.log("%j",global.config);
 var check_calander = new cornJob('* * * * *',function(){
 	console.log("check calander");
 	var option = {
@@ -62,10 +46,7 @@ var check_calander = new cornJob('* * * * *',function(){
 		// uncomment it for header details
 		//  console.log("headers: ", res.headers);
 		res.on('data', function(data) {
-			//console.info('GET result:\n');
 			reply += data;
-			//process.stdout.write(d);
-			//console.info('\n\nCall completed');
 		});
 		res.on('end',function(){
 			try{
@@ -99,15 +80,12 @@ var check_calander = new cornJob('* * * * *',function(){
 				console.log("updated, last calander id: "+ calander_last_id);
 				watchdog_util.sync(data_file_root);
 			}
-			//console.log(global.calander_pool);
-			//console.log(global.config);
 		});
 	});
 	reqGet.end();
 	reqGet.on('error', function(e) {
 		console.error(e);
 	});
-	//console.log("test");
 });
 check_calander.start();
 
